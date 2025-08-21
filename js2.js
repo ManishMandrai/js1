632c050c543d1e73919108b512994e12a40b5449
ManishMandrai <manishkumarmandrai@gmail.com>
2025-08-21 23:42:29 +0530

    done

---
+++ js2.js
@@ -0,0 +1,53 @@
+// let num1 = 5
+// let num2 = 8
+
+// console.log("I am a regular upper code");
+
+// if (num1 > num2){
+//     console.log("num1 is greater then num2");
+// }else{
+//     console.log("Nope, num1 is NOT greater");
+    
+// }
+// console.log("I am regular bottom code");
+
+// let userName = "Mango";
+// let anotherUsername = "Tango";
+
+// if(userName == anotherUsername) {
+//     console.log("Pick another Username");   
+// }else{
+//     console.log("You can pick this one");
+// }
+
+
+
+// let score = 264
+
+// if (typeof score === 'number'){
+//     console.log("Yup, This is a number");
+// }else{
+//     console.log("not a number");
+    
+// }
+
+
+// let isTeaReady = true
+
+// if(isTeaReady){
+//     console.log("Tea is ready");
+// }else{
+//     console.log("Tea is not ready");
+// }
+
+
+let items = ["dog", "cat"]
+
+console.log(items.length);
+
+
+if(items.length === 0){
+    console.log("Array is empty");
+}else{
+    console.log("Arrey is not empty");
+}
