5fe17edf0ef27be8baaf54dd884884da9e845bae
ManishMandrai <manishkumarmandrai@gmail.com>
2025-08-21 23:42:41 +0530

    ok

--- js2.js
+++ js2.js
@@ -1,44 +1,44 @@
-// let num1 = 5
-// let num2 = 8
+let num1 = 5
+let num2 = 8
 
-// console.log("I am a regular upper code");
+console.log("I am a regular upper code");
 
-// if (num1 > num2){
-//     console.log("num1 is greater then num2");
-// }else{
-//     console.log("Nope, num1 is NOT greater");
+if (num1 > num2){
+    console.log("num1 is greater then num2");
+}else{
+    console.log("Nope, num1 is NOT greater");
     
-// }
-// console.log("I am regular bottom code");
+}
+console.log("I am regular bottom code");
 
-// let userName = "Mango";
-// let anotherUsername = "Tango";
+let userName = "Mango";
+let anotherUsername = "Tango";
 
-// if(userName == anotherUsername) {
-//     console.log("Pick another Username");   
-// }else{
-//     console.log("You can pick this one");
-// }
+if(userName == anotherUsername) {
+    console.log("Pick another Username");   
+}else{
+    console.log("You can pick this one");
+}
 
 
 
-// let score = 264
+let score = 264
 
-// if (typeof score === 'number'){
-//     console.log("Yup, This is a number");
-// }else{
-//     console.log("not a number");
+if (typeof score === 'number'){
+    console.log("Yup, This is a number");
+}else{
+    console.log("not a number");
     
-// }
+}
 
 
-// let isTeaReady = true
+let isTeaReady = true
 
-// if(isTeaReady){
-//     console.log("Tea is ready");
-// }else{
-//     console.log("Tea is not ready");
-// }
+if(isTeaReady){
+    console.log("Tea is ready");
+}else{
+    console.log("Tea is not ready");
+}
 
 
 let items = ["dog", "cat"]
